b946eda7b449c7c3ea40161aef8942cc06e59c38
Nasar-uddin <nasaruddin171@gmail.com>
2020-05-01 18:12:24 +0600

    pyany three

--- NAZADV/settings.py
+++ NAZADV/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'NAZADV/static')
+    '/home/nasar007/naz-adv-final/static'
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, '/home/nasar007/naz-adv-final/static')
 STATIC_URL = '/static/'
