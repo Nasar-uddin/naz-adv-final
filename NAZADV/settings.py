c6d6b9f1caa74c2c9f26c86de87686eef66a7fba
Nasar-uddin <nasaruddin171@gmail.com>
2020-05-01 17:58:49 +0600

    in pyevw edit one

--- NAZADV/settings.py
+++ NAZADV/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'NAZADV/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/home/nasar007/naz-adv-final/static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL  = '/media/'
+MEDIA_URL  = '/home/nasar007/naz-adv-final/media/'
